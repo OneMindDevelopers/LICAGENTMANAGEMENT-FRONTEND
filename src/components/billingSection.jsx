46dacd20d2adda15ceb5b100c90af4a2b8eb0e83
abhishek <oneminddevelopers@gmail.com>
2023-06-24 11:14:04 +0530

    commting confirmation page

--- src/components/billingSection.jsx
+++ src/components/billingSection.jsx
@@ -66,21 +66,83 @@ const BillingSectionComponent = ({ OnEditOptionBillingPage, history }) => {
 
   const navigateToconfirmComponent = () => {
     history.push("/confirm");
- 
-  }
+  };
+
+  console.log("billingItems", billingItems);
 
   return (
     <React.Fragment>
       <h1>Order Summary</h1>
       <div className="row">
-        <div className="jumbotron  mt-5 col-6 ">
-          <button
-            className="editbutton_billingpage"
-            onClick={handleEditOptionBillingPage}
-          >
-            Edit the Selected Items
-          </button>
-          {billingItems.map((billingItem) => (
+        <div className="jumbotron  mt-5 col-6">
+          <span className="billing-edit-page">
+            <button
+              className="editbutton_billingpage"
+              onClick={handleEditOptionBillingPage}
+            >
+              Edit the Selected Items
+            </button>
+          </span>
+
+          <span className="billing-navigate-confirmation">
+            <button onClick={navigateToconfirmComponent}>
+              Continue to Confirmation Page
+            </button>
+          </span>
+
+          <ul className="list-group">
+            {billingItems.map((billingItem) => (
+              <li key={billingItem.id} className="list-group-item">
+                <span className="billing-item-name">
+                  Name:{billingItem.name}
+                </span>
+                <span className="billing-qty-icons">
+                  <p className="card-text">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="26"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-file-plus-fill"
+                      viewBox="0 0 16 16"
+                      onClick={() => {
+                        handlePlusIncrement(billingItem);
+                      }}
+                    >
+                      <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM8.5 6v1.5H10a.5.5 0 0 1 0 1H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 0 1 0-1h1.5V6a.5.5 0 0 1 1 0z" />
+                    </svg>
+                    <input
+                      type="number"
+                      className="width-40"
+                      value={billingItem.quantity}
+                    />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      class="bi bi-file-minus-fill"
+                      viewBox="0 0 16 16"
+                      onClick={() => {
+                        //this.handleDeletionItem(item);
+                        handleMinusDecrement(billingItem);
+                      }}
+                    >
+                      <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM6 7.5h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1 0-1z" />
+                    </svg>
+                  </p>
+                </span>
+
+                <span className="billing-item-size">
+                  SIZE:{billingItem.size}
+                </span>
+                <span className="billing-item-price">
+                  PRICE:{billingItem.price}
+                </span>
+              </li>
+            ))}
+          </ul>
+          {/* {billingItems.map((billingItem) => (
             <div
               className="card billing-card-width float-left m-5"
               key={billingItem._id}
@@ -125,7 +187,7 @@ const BillingSectionComponent = ({ OnEditOptionBillingPage, history }) => {
                 <p>Price: {billingItem.price}</p>
               </div>
             </div>
-          ))}
+          ))} */}
         </div>
         <div className="col-6 background-white">
           <h1> Pricing Details:</h1>
@@ -135,9 +197,6 @@ const BillingSectionComponent = ({ OnEditOptionBillingPage, history }) => {
           <p> Total Price: {totalPrice}</p>
         </div>
       </div>
-      <button onClick={navigateToconfirmComponent}>
-          Continue to Confirmation Page
-        </button>
     </React.Fragment>
   );
 };
